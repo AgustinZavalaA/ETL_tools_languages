4bfb6b2c24f22cfd1297b26dc10669046fd5b7dc
AgustinZavalaA <1930120@upv.edu.mx>
2022-06-17 09:42:27 -0500

    eliminador de columnas y de nulos

---
+++ eliminador_columnas.py
@@ -0,0 +1,19 @@
+import sys
+import pandas as pd
+import numpy as np
+
+
+def main(csv_filename: str, columns_to_save: list[str], replace_file: bool = True) -> None:
+    print("Eliminador de columnas")
+    # load csv
+    df = pd.read_csv(csv_filename)
+    # get columns to save
+    df = df[columns_to_save]
+    # save csv
+    if replace_file:
+        df.to_csv(f"{csv_filename.split('.')[0]}_col.csv", index=False)
+
+
+if __name__ == "__main__":
+    print(sys.argv)
+    main(sys.argv[1], sys.argv[2:])
---
+++ eliminador_nulos.py
@@ -0,0 +1,33 @@
+import sys
+import pandas as pd
+import numpy as np
+import enum
+
+
+class NullCheckerType(enum.Enum):
+    check_null = "check_null"
+    check_zero = "check_zero"
+    check_negative = "check_negative"
+    check_empty_str = "check_empty_str"
+
+
+def main(csv_filename: str, types_to_check: list[str]) -> None:
+    print("Eliminador de filas con valores nulos")
+    # load csv
+    df = pd.read_csv(csv_filename)
+    if NullCheckerType.check_zero.name in types_to_check:
+        df = df.replace(0, np.nan)
+    if NullCheckerType.check_negative.name in types_to_check:
+        # df[ < 0] = np.nan
+        df[df[df.select_dtypes([np.number]).columns] < 0] = np.nan
+    if NullCheckerType.check_empty_str.name in types_to_check:
+        df = df.replace("^[^\w]$", np.nan, regex=True)
+
+    # eliminate rows with null values
+    df = df.dropna(axis=0, how="any")
+    # save csv
+    df.to_csv(f"{csv_filename.split('.')[0]}_null.csv", index=False)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2:])
